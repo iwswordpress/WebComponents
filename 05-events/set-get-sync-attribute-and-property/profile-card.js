fc6d761a1284662dd3dc631b117abd0c0da05b61
iwswordpress <iwswordpress@gmail.com>
2020-05-18 12:57:42 +0100

    edit set-get loop

--- 05-events/set-get-sync-attribute-and-property/profile-card.js
+++ 05-events/set-get-sync-attribute-and-property/profile-card.js
@@ -1,61 +1,45 @@
 export class ProfileCard extends HTMLElement {
     constructor() {
         super();
-
-        this.firstName = 'John';
-        this.lastName = 'Doe';
-        this.fullName = this.firstName + ' ' + this.lastName;
-
+        this._firstName;
+        this._lastName;
+        this._fullName;
         this.attachShadow({
             mode: 'open'
         });
     }
-
     connectedCallback() {
         // Change a property and see it reflected as an attribute
-        this.firsttName = 'Tony';
-        this.lastName = 'Adamson';
+        this._fullName = this._firstName + ' ' + this._lastName;
         this.render();
     }
-
-
     render() {
         this.shadowRoot.innerHTML = `
-            <img src='https://placeimg.com/100/100/people' />
-
-            <h1>${this.firstName} : ${this.lastName}</h1>
-            
-            <h1>${this.fullName}</h1>
-            <p>Title</p>
+            <img src='https://placeimg.com/100/100/people' />       
+            <h1>${this._fullName}</h1>
         `;
     }
-
     attributeChangedCallback(name, oldValue, newValue) {
-
         // if we were to set property firstName to new attribute value it would fire off SET which in
         // turn fires of atributeChangedCallback - an endless loop that gives an out of memory error.
-
         // if we have set attributes on component in html then we can do this.firstName = newValue;
         // and we then don't need getters and setters.
-
         // NB This gets fired initially as attribute is null but we set firstName and lastName in constror which tiggers the set method
-        if (name === 'the-first-name' || name == 'the-last-name') {
-            //this.firstName = newValue; endless loop
-            this.fullName = `${this.firstName} ${this.lastName}`;
-            console.log("[attributeChangedCallback] FIRST " + this.firstName);
-            console.log("[attributeChangedCallback] LAST " + this.lastName);
-            console.log("[attributeChangedCallback] " + this.firstName + " --> " + this.lastName + " Fullname is now: " + this.fullName);
+        if (name === 'the-first-name') {
+            //this._firstName = newValue; //endless loop
         }
-
+        if (name === 'the-last-name') {
+            // this._lastName = newValue; //endless loop
+        }
+        this._fullName = this._firstName + ' ' + this._lastName;
+        console.log(this._fullName)
         this.render();
     }
-
     static get observedAttributes() {
         return ['the-first-name', 'the-last-name'];
     }
-
     // set  means that when we change the firstName we also change the atribute to be in sync
-    set firstName(val) {
+    set _firstName(val) {
         if (val) {
             this.setAttribute('the-first-name', val);
             console.log("firstName setter")
@@ -65,13 +49,11 @@ export class ProfileCard extends HTMLElement {
     }
     // get  means if use/call firstName it uses the-first-name attribute value.
     // getters and setters make fns into properties so get firstName() === this.firstName
-    get firstName() {
+    get _firstName() {
         console.log("firstName getter")
         return this.getAttribute('the-first-name');
-
     }
-
-    set lastName(val) {
+    set _lastName(val) {
         if (val) {
             this.setAttribute('the-last-name', val);
             console.log("lastName setter")
@@ -79,11 +61,9 @@ export class ProfileCard extends HTMLElement {
             this.removeAttribute('the-last-name');
         }
     }
-
-    get lastName() {
+    get _lastName() {
         console.log("lastName getter")
         return this.getAttribute('the-last-name');
     }
 }
-
 customElements.define('profile-card', ProfileCard);
