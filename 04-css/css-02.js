bd4044fa2bc819de0f0f6b0e65858622a88344a5
= <=>
2020-05-20 11:11:52 +0100

    edit import css via js

--- 04-css/css-02.js
+++ 04-css/css-02.js
@@ -4,7 +4,7 @@ export const CSS = `
       border: 3px solid red;
       padding:20px;
       margin-bottom:20px;
-      background: var(--background-color-01, yellow);
+      background: var(--background, yellow);
       font-size: var(--main-font-size, 20px);
       font-family: var(--main-font, inherit);
       color: var(--main-text-color, red);
