5aff10ac87203f751a920c527b877d46430048d1
iwswordpress <iwswordpress@gmail.com>
2020-10-17 08:09:04 +0100

    changes in 05 during recording

--- 05-events/15-child-to-parent.js
+++ 05-events/15-child-to-parent.js
@@ -21,23 +21,26 @@ class ChildOne extends HTMLElement {
     });
     // true means deep clone
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.postListElement = this.shadowRoot.querySelector('section');
   }
   connectedCallback() {
     const btn = this.shadowRoot.getElementById('btn');
-    btn.addEventListener('click', (e) => {
-      console.log("[CHILD ONE JS] +++++ CLICK START +++++");
+    btn.addEventListener('click', e => {
+      console.log('[CHILD ONE JS] +++++ CLICK START +++++');
       console.log(e);
-      const eventData = 'DATA SENT IN CUSTOM EVENT ' + Math.floor(Math.random() * 1000);
-      // ++++++++++ CUSTOM EVENTS ++++++++++ 
-      this.dispatchEvent(new CustomEvent('childOneClick', { // We can choose the name of our event
-        detail: eventData, // we can pass data - variables or objects
-        bubbles: true, // by default Web Components do not bubble events unlike regular DOM events
-        composed: true // this is needed to escape the Shadow DOM encapsulation and be heard by the Light DOM
-      }));
-      // ++++++++++ CUSTOM EVENTS ++++++++++ 
-      console.log("[CHILD ONE JS] +++++ CLICK END +++++ ");
+      const eventData =
+        'DATA SENT IN CUSTOM EVENT ' + Math.floor(Math.random() * 1000);
+      // ++++++++++ CUSTOM EVENTS ++++++++++
+      this.dispatchEvent(
+        new CustomEvent('childOneClick', {
+          // We can choose the name of our event
+          detail: eventData, // we can pass data - variables or objects
+          bubbles: true // by default Web Components do not bubble events unlike regular DOM events
+          // composed: true // this is needed to escape the Shadow DOM encapsulation and be heard by the Light DOM
+        })
+      );
+      // ++++++++++ CUSTOM EVENTS ++++++++++
+      console.log('[CHILD ONE JS] +++++ CLICK END +++++ ');
     });
-  };
+  }
 }
 customElements.define('child-one', ChildOne);
